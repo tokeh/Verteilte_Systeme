d91f481d547422f3aa954e51ae4182f9d3d2d2a0
tobi <tobias.keh@googlemail.com>
2013-04-10 10:20:37 +0200

    LeafDirectory is now able to store several entites to one cell.

--- aufgabe3/src/main/java/blatt3/aufgabe/LeafDirectory.java
+++ aufgabe3/src/main/java/blatt3/aufgabe/LeafDirectory.java
@@ -1,28 +1,31 @@
 package blatt3.aufgabe;
 
+import java.util.HashMap;
+
 public class LeafDirectory extends Directory {
 
 	private CompositeDirectory myParent;
+	private HashMap<Integer, Cell> myEntities;
 	private Cell myCell;
-	private int myEntity;
 
 	protected LeafDirectory(Cell cell, CompositeDirectory parent) {
 		super(parent);
-		this.myCell = cell;
 		this.myParent = parent;
+		this.myEntities = new HashMap<Integer, Cell>();
+		this.myCell = cell;
 	}
 
 	@Override
 	Cell lookup(int entity) {
-		if (entity == this.myEntity) {
-			return this.myCell;
+		if (this.myEntities.containsKey(entity)) {
+			return this.myEntities.get(entity);
 		} else {
 			return this.myParent.lookup(entity);
 		}
 	}
 
 	public void insert(int entity) {
-		this.myEntity = entity;
+		this.myEntities.put(entity, this.myCell);
 		this.myParent.addChild(entity, this);
 	}
 
